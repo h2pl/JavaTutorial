f402c30e4522b4ef853eadabd7aaf19baea15c6f
黄小斜 <362294931@qq.com>
2019-11-22 13:49:08 +0800

    auto create toc

--- src/main/java/md/mdToc.java
+++ src/main/java/md/mdToc.java
@@ -4,7 +4,7 @@ import com.github.houbb.markdown.toc.core.impl.AtxMarkdownToc;
 
 public class mdToc {
     public static void main(String[] args) {
-        String path = "D:\\javaTutorial\\docs\\distrubuted\\practice\\";
+        String path = "D:\\javaTutorial\\docs\\";
         AtxMarkdownToc.newInstance().genTocDir(path);
     }
 }
